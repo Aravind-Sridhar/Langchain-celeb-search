5b9b28ccc201a88d9a07ce48349b2e92225f57e8
Aravind Sridhar <aravindsri30@gmail.com>
2023-12-07 14:25:22 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ os.environ['OPENAI_API_KEY'] = openai_key
 
 st.title('LangChain Demo with OpenAI API')
 input_text= st.text_input("Search topic of interest")
+
+llm = OpenAI(temperature = 0.8)
+
+if input_text:
+    st.write(llm(input_text))
